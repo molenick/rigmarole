d6ec4ea47af8e809ee2856c3983b64ec777ae949
mo <molenick@users.noreply.github.com>
2024-03-24 14:42:11 -0700

    Improve

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,5 @@
 #!/bin/sh
 
 set -e
-
-# TODO Need to learn how github actually wants to handle path instead of this
-# hardcoding.
-git config --global --add safe.directory "/github/workspace/$INPUT_PATH"
-
-echo "$INPUT_PATH"
-
 cd "$INPUT_PATH"
-
 license_finder
